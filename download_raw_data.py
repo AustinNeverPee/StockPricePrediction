ef3fae6b96d6f81118a78c329fd644bd87d0f11b
AustinNeverPee <austin_never_pee@foxmail.com>
2017-05-06 22:11:33 +0800

    Add raw data download script

---
+++ download_raw_data.py
@@ -0,0 +1,23 @@
+"""Download raw data and save as pickle format
+"""
+
+
+import pandas as pd
+import pytz
+from datetime import datetime
+from zipline.utils.factory import load_bars_from_yahoo
+import pickle
+
+
+# Load data manually from Yahoo! finance
+start = datetime(2009, 1, 1, 0, 0, 0, 0, pytz.utc)
+end = datetime(2016, 1, 1, 0, 0, 0, 0, pytz.utc)
+data = load_bars_from_yahoo(stocks=['AAPL'],
+                            start=start,
+                            end=end)
+
+# Store raw data into pickle format
+output = open('raw data/AAPL.pkl', 'wb')
+# Pickle dictionary using protocol 0.
+pickle.dump(data, output)
+output.close()
